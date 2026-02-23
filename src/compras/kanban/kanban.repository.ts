ac0077c1ea2526615e761b741b11fd01c07a310a
Develop-Ac <develop@acacessorios.com.br>
2026-02-23 10:34:41 -0400

    1.1.10

--- src/compras/kanban/kanban.repository.ts
+++ src/compras/kanban/kanban.repository.ts
@@ -11,6 +11,7 @@ export class kanbanRepository {
 
   async update(id: string, data: any) {
       // Envia apenas o campo 'data' para o Prisma
+      console.log('Updating kanban with id:', id, 'and data:', data);
       return this.prisma.com_kanban.update({
         where: { id },
         data: { data: data.data }
